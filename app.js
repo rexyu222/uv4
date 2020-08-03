0b3aa849d84666b514203180a2db3e6f722f9fab
rexyu222 <rexyu222@gmail.com>
2020-08-03 16:44:42 -0400

    rex nodeExpress3 2 version

--- app.js
+++ app.js
@@ -1,27 +1,11 @@
-// Get dependencies
 const express = require('express');
-//const path = require('path');
-
 const app = express();
 
 
 app.get('/', (req, res) => {
-//  res.sendFile(path.join(__dirname, 'dist/rexTechAObservableVsNGRX/index.html'));
- // res.sendFile(path.join(__dirname, 'dist/usEnermyscss6/index.html'));
     res.send("rex homepage");
 });
-/**
- * Get port from environment and store in Express.
- */
-const port = process.env.PORT || '3000';
-//app.set('port', port);
-/**
- * Create HTTP server.
- */
-//var server = http.createServer(app);
-/**
- * Listen on provided port, on all network interfaces.
- */
-//server.listen(port, () => console.log(`API running on localhost:${port}`));
 
-app.listen(port, () => {console.log(`API running on localhost:${port}`);});
+const port = process.env.PORT || 3000;
+
+app.listen(port, () => {console.log("API running on localhost");});
