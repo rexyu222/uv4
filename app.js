12f754b70ddc6465561c9b4cd1c0407ce3dc10ae
rexyu222 <rexyu222@gmail.com>
2020-08-03 19:24:09 -0400

    rex nodeExpress3 6 version

--- app.js
+++ app.js
@@ -1,2 +1,11 @@
 const express = require('express');
 const app = express();
+
+
+app.get('/', (req, res) => {
+    res.send("rex homepage");
+});
+
+const port = process.env.PORT || 3000;
+
+app.listen(port, () => {console.log("API running on localhost");});
