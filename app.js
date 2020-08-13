692164684a28a8d51560b1bedca07e058f8ea76e
rexyu222 <rexyu222@gmail.com>
2020-08-13 19:18:30 -0400

    rex 20 version

--- app.js
+++ app.js
@@ -13,24 +13,8 @@ app.get('/', (req, res) => {
 });
 
 
-
-//debug begin
 const bodyParser= require('body-parser');
 const jsonParser= bodyParser.json();
-/**const fs = require("fs");
-console.log('rex post2: fs begin ');
-
-  // var commentInfo= '\r\n' + req.body.message + '   SaveTime: ' + new Date().toLocaleString("en-US", {timeZone: "America/New_York"});
-  var commentInfo= '\r\n'  + '\r\n'  + '   SaveTime: ' + new Date().toLocaleString("en-US", {timeZone: "America/New_York"})+'\r\n' + 'rex2commentusroute';
-  fs.appendFileSync('customerComment.txt', commentInfo, function(err){
-    if (err) throw err;
-    console.log('Saved');
-  });
-  console.log('rex post2: fs end ');**/
-//debug end  
-
-//set commentUs routes
-app.use('/urlLogsave', require('./urlLogRoutes/urlLogRoute'));
 
 //set commentUs routes
 //app.use('/commentussave', require('./commentUsRoutes/commentUsRoute'));
@@ -52,6 +36,21 @@ app.post('/commentussave', jsonParser, function(req, res)  {
   //res.send(200,  req.body);
   res.status(200).send(req.body);
 });
+
+
+//set commentUs routes
+app.post('/urlLogsave', jsonParser, function(req, res)  {
+  var urlLogInfo= '\r\n'   + '   SaveTime: ' + new Date().toLocaleString("en-US", {timeZone: "America/New_York"})+'\r\n' + JSON.stringify(req.body);
+  fs.appendFileSync('urlLog.txt', urlLogInfo, function(err){
+    if (err) throw err;
+    console.log('Saved');
+  });
+
+ // res.sendStatus(200);
+  //res.send(200,  req.body);
+  res.status(200).send(req.body);
+});
+
 
 const port = process.env.PORT || 3000;
 
--- urlLogRoutes/urlLogRoute.js
+++ urlLogRoutes/urlLogRoute.js
@@ -11,20 +11,20 @@ var fs = require("fs");
 // console.log("rex put");
   //res.send('rexapi works');
 //});
-router.route('/').post( (req, res) => {
+// router.route('/').post( (req, res) => {
 
-  console.log('rex post2 urlLog: '+ JSON.stringify(req.body));
-  //console.log('rex post2 urlLog: '+ req.body);
-  // var commentInfo= '\r\n' + req.body.message + '   SaveTime: ' + new Date().toLocaleString("en-US", {timeZone: "America/New_York"});
- // var commentInfo= '\r\n'  + '\r\n'  + '   SaveTime: ' + new Date().toLocaleString("en-US", {timeZone: "America/New_York"})+'\r\n' + JSON.stringify(req.body);
- // fs.appendFileSync('urlLog.txt', ('\r\n'  +JSON.stringify(req.body.urlLog)), function(err){
-  fs.appendFileSync('urlLog.txt', ('\r\n'  +JSON.stringify(req.body)), function(err){
-    if (err) throw err;
-    console.log('Saved');
-  });
+//   console.log('rex post2 urlLog: '+ JSON.stringify(req.body));
+//   //console.log('rex post2 urlLog: '+ req.body);
+//   // var commentInfo= '\r\n' + req.body.message + '   SaveTime: ' + new Date().toLocaleString("en-US", {timeZone: "America/New_York"});
+//  // var commentInfo= '\r\n'  + '\r\n'  + '   SaveTime: ' + new Date().toLocaleString("en-US", {timeZone: "America/New_York"})+'\r\n' + JSON.stringify(req.body);
+//  // fs.appendFileSync('urlLog.txt', ('\r\n'  +JSON.stringify(req.body.urlLog)), function(err){
+//   fs.appendFileSync('urlLog.txt', ('\r\n'  +JSON.stringify(req.body)), function(err){
+//     if (err) throw err;
+//     console.log('Saved');
+//   });
 
- // res.sendStatus(200);
-  //res.send(200,  req.body);
-  res.status(200).send(req.body);
-})
-module.exports = router;
+//  // res.sendStatus(200);
+//   //res.send(200,  req.body);
+//   res.status(200).send(req.body);
+// })
+// module.exports = router;
