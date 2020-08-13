39a3519c32c3a12f38b90a075eb0290f97d0fafa
rexyu222 <rexyu222@gmail.com>
2020-08-13 13:16:16 -0400

    rex 13 version

--- app.js
+++ app.js
@@ -29,8 +29,24 @@ console.log('rex post2: fs begin ');
 app.use('/urlLogsave', require('./urlLogRoutes/urlLogRoute'));
 
 //set commentUs routes
-app.use('/commentussave', require('./commentUsRoutes/commentUsRoute'));
-console.log("API running on localhost nodejs");
+//app.use('/commentussave', require('./commentUsRoutes/commentUsRoute'));
+app.post('/commentussave', function(req, res)  {
+  
+  console.log('rex post2: rex null');
+  console.log('rex post2: '+ JSON.stringify(req.body));
+
+  // var commentInfo= '\r\n' + req.body.message + '   SaveTime: ' + new Date().toLocaleString("en-US", {timeZone: "America/New_York"});
+  var commentInfo= '\r\n'  + '\r\n'  + '   SaveTime: ' + new Date().toLocaleString("en-US", {timeZone: "America/New_York"})+'\r\n' + JSON.stringify(req.body);
+  fs.appendFileSync('customerComment.txt', commentInfo, function(err){
+    if (err) throw err;
+    console.log('Saved');
+  });
+
+ // res.sendStatus(200);
+  //res.send(200,  req.body);
+  res.status(200).send(req.body);
+});
+
 const port = process.env.PORT || 3000;
 
 app.listen(port, () => {console.log("API running on localhost");});
--- commentUsRoutes/commentUsRoute.js
+++ commentUsRoutes/commentUsRoute.js
@@ -11,20 +11,20 @@ var fs = require("fs");
 // console.log("rex put");
   //res.send('rexapi works');
 //});
-router.route('/').post( (req, res) => {
+// router.route('/').post( (req, res) => {
   
-  console.log('rex post2: rex null');
-  console.log('rex post2: '+ JSON.stringify(req.body));
+//   console.log('rex post2: rex null');
+//   console.log('rex post2: '+ JSON.stringify(req.body));
 
-  // var commentInfo= '\r\n' + req.body.message + '   SaveTime: ' + new Date().toLocaleString("en-US", {timeZone: "America/New_York"});
-  var commentInfo= '\r\n'  + '\r\n'  + '   SaveTime: ' + new Date().toLocaleString("en-US", {timeZone: "America/New_York"})+'\r\n' + JSON.stringify(req.body);
-  fs.appendFileSync('customerComment.txt', commentInfo, function(err){
-    if (err) throw err;
-    console.log('Saved');
-  });
+//   // var commentInfo= '\r\n' + req.body.message + '   SaveTime: ' + new Date().toLocaleString("en-US", {timeZone: "America/New_York"});
+//   var commentInfo= '\r\n'  + '\r\n'  + '   SaveTime: ' + new Date().toLocaleString("en-US", {timeZone: "America/New_York"})+'\r\n' + JSON.stringify(req.body);
+//   fs.appendFileSync('customerComment.txt', commentInfo, function(err){
+//     if (err) throw err;
+//     console.log('Saved');
+//   });
 
- // res.sendStatus(200);
-  //res.send(200,  req.body);
-  res.status(200).send(req.body);
-})
-module.exports = router;
+//  // res.sendStatus(200);
+//   //res.send(200,  req.body);
+//   res.status(200).send(req.body);
+// })
+// module.exports = router;
